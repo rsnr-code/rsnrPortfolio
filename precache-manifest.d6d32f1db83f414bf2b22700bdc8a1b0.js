1ce28031f1e28b790fbee29e281d9cfa5976b69b
rsnr-code <rishadnero@gmail.com>
2022-11-10 16:58:21 -0500

    Updates

---
+++ precache-manifest.d6d32f1db83f414bf2b22700bdc8a1b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e8bfa7abda0f6783a0c4b197141181e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3d4c1d56635844be47d",
+    "url": "/static/css/2.e45429e2.chunk.css"
+  },
+  {
+    "revision": "b38c0bd9a641a1079fb1",
+    "url": "/static/css/main.6c3d1e78.chunk.css"
+  },
+  {
+    "revision": "e3d4c1d56635844be47d",
+    "url": "/static/js/2.0a7ea554.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.0a7ea554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b38c0bd9a641a1079fb1",
+    "url": "/static/js/main.148dc72a.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
